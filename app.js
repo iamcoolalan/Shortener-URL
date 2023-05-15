0da1f1db9b2a58404f9eb58aae2c540061359eb7
iamcoolalan <iamalan1996@gmail.com>
2023-05-15 16:19:50 +0800

    feat: add main view
    - setting view engine
    - setting home routes
    - add simple view for home page

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+//require packages used in project
 const express = require('express')
+const exhbs = require('express-handlebars')
+
+const routes = require('./routes')
+
 const app = express()
-
-
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hi')
-})
+//setting view engine
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
+//setting routes
+app.use(routes)
+
+//start and listen on the Express server
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
